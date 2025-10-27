812b80ea126fab2dd1a4898456996da8ea4b03b0
OscarJohnson2005 <146839647+OscarJohnson2005@users.noreply.github.com>
2025-10-27 20:08:18 +0000

    Update Main.kt

--- portfolio/wordle/src/main/kotlin/Main.kt
+++ portfolio/wordle/src/main/kotlin/Main.kt
@@ -1,3 +1,24 @@
-fun main() {
+fun main() 
+{
+  val words = readWordList("data/words.txt")
+  val target = pickRandomWord(words)
+
+  println("Wordle!")
+  println("Guess the five letter word. You have six attempts.")
+
+  for (attempt in 1..6)
+  {
+    val guess = obtaingGuess(attempt)
+    val matches = evaluateGuess(guess, target)
+    displayGuess(guess, matches)
+
+    if (matches.all{it == 2})
+    {
+      println("Congratulations! You correctly guessed: $target")
+      return
+    }
+  }
+  
+  println("All attempts used. The word was: $target")
   
 }
